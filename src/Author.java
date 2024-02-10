8b10cfca7875773506dcd9fd3de0c7835df4093d
Владимир <vkuznecov1504@mail.ru>
2024-02-10 16:29:43 +0400

    Написали констуркотры для класов Book и Author

--- src/Author.java
+++ src/Author.java
@@ -2,4 +2,8 @@ public class Author {
     String name;
     String surname;
 
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -2,4 +2,9 @@ public class Book {
    String bookTitle;
    int year;
 
+   public Book(String bookTitle, int year) {
+      this.bookTitle = bookTitle;
+      this.year = year;
+   }
+
 }
