2fc20385a9e00d029fb01e9dc78881feab08f568
Владимир <vkuznecov1504@mail.ru>
2024-02-10 16:53:05 +0400

    Создали геттеры для всех полей автора и всех полей книги, добавили модификатор доступа private

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,18 @@
 public class Book {
-   String bookTitle;
-   int year;
+   private String bookTitle;
+   private int year;
 
    public Book(String bookTitle, int year) {
       this.bookTitle = bookTitle;
       this.year = year;
+   }
+
+   public String getBookTitle() {
+      return this.bookTitle;
+   }
+
+   public int getYear() {
+      return this.year;
    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
+        Book book = new Book("Green mile", 1996);
+        Author author = new Author("Stiven", "King");
+        System.out.println("book.bookTitle = " + book.getBookTitle());
+        System.out.println("book.year = " + book.getYear());
+        System.out.println("author.name = " + author.getName());
+        System.out.println("author.surname = " + author.getSurname());
+
     }
 }
